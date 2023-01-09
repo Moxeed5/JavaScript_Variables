4f96a9b8154a0349643354604302821dcff4eda4
Max Morgan <maxmmorgan93@gmail.com>
2023-01-09 11:07:50 -0600

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,52 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Max";
+
+let lastName;
+
+var age;
+
 // Exercise 2
 
+lastName = "Morgan"
+
+age = 29;
+
 // Exercise 3
+
+
+let language = "Javascript";
+
+console.log(language);
+
+let createdYear = 1995;
+
+let isCaseSensitive = true;
+
+// Exercise 4
+
+let price = 19.99;
+
+let isOnSale = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+//Exercise 5
+
+let title = "Name of the wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookMark = 456;
+
+let hasRead = true;
+
